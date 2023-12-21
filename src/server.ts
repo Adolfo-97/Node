a5040c124a371cfdc5aad8b043451f291ee9ac74
Adolfo-97 <adolfogomez@live.it>
2023-12-21 19:49:50 +0100

    upload esercizio

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,34 @@ app.get("/", (req, res) => {
 app.get("/planets", (req, res) => {
   res.status(200).json(planets);
 });
+app.get("/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet);
+});
+app.post("/planets/", (req, res) => {
+  const { id, name } = req.body;
+  const stringa = req.body;
+  console.log(`ho ricevuto la request: ${req}`);
+  console.log(`ho ricevuto il body: ${stringa}`);
+  const newPlanet = { id, name };
+
+  planets = [...planets, newPlanet];
+  res.status(201).json({ msg: "the planets has been added" });
+});
+
+app.put("/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+  res.status(200).json({ msg: "Planet has been updated" });
+});
+
+app.delete("/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+  res.status(200).json({ msg: "Planet deleted" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
