62f80e5673c0b90ee9c81161d93ecd48b49d5ed3
Adolfo-97 <adolfogomez@live.it>
2024-01-13 16:35:56 +0100

    upload

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+} from "./controllers/planets.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,59 +17,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "hello world" });
-});
-
-app.get("/planets", (req, res) => {
-  res.status(200).json(planets);
-});
-app.get("/planets/:id", (req, res) => {
-  const { id } = req.params;
-  const planet = planets.find((p) => p.id === Number(id));
-  res.status(200).json(planet);
-});
-app.post("/planets/", (req, res) => {
-  const { id, name } = req.body;
-  const stringa = req.body;
-  console.log(`ho ricevuto la request: ${req}`);
-  console.log(`ho ricevuto il body: ${stringa}`);
-  const newPlanet = { id, name };
-
-  planets = [...planets, newPlanet];
-  res.status(201).json({ msg: "the planets has been added" });
-});
-
-app.put("/planets/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
-  res.status(200).json({ msg: "Planet has been updated" });
-});
-
-app.delete("/planets/:id", (req, res) => {
-  const { id } = req.params;
-  planets = planets.filter((p) => p.id !== Number(id));
-  res.status(200).json({ msg: "Planet deleted" });
-});
+app.get("/planets", getAll);
+app.get("/planets/:id", getOneById);
+app.post("/planets/", create);
+app.put("/planets/:id", updateById);
+app.delete("/planets/:id", deleteById);
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 });
-type Planet = {
-  id: number;
-  name: string;
-};
-
-type Planets = Planet[];
-
-let planets: Planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
